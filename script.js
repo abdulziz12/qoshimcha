278f9b65710ebc84a82b49afe3e0eb3d8c03028f
abdulziz12 <abdurashidov200930@gmail.com>
2024-06-22 13:27:44 +0500

    kiss
    agshtb

---
+++ script.js
@@ -0,0 +1,23 @@
+let ism = prompt('ismingizni kiriting')
+
+if (ism ==='abdulaziz'){
+    alert( 'saytga xush kelibsiz')
+}
+ else if (ism === 'qodirali'){
+    alert('saytga xush kelibsiz')
+}
+else if (ism === 'abbosxonjonbek'){
+    alert('saytga xush kelibsiz')
+}
+else if (ism === 'ustoz'){
+    alert('saytga xush kelibsiz')
+}
+else if (ism === 'sanjar'){
+    alert('saytga xush kelibsiz')
+}
+else if (ism === 'sotiqo`zi'){
+    alert('saytga xush kelibsiz')
+}
+else{ 
+    alert('sayitga kirish mumkin emas')
+}
